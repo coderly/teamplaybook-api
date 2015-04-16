5381720a9a7e19b9c2493f99db3c970ee5270ad4
israel de la hoz <israeldelahoz@gmail.com>
2015-04-16 15:20:18 -0500

    load organization, handle 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+  before_filter :fetch_organization
+
+  NON_ORGANIZATION_SUBDOMAINS = [:www]
+
+  protected
+
+  def fetch_organization
+    @organization = Organization.find_by! subdomain: request.subdomain if has_organization_subdomain?
+  end
+
+  private
+
+  def has_organization_subdomain?
+    request.subdomain.present? && !NON_ORGANIZATION_SUBDOMAINS.include?(request.subdomain.to_sym)
+  end
+
+  def not_found
+    render json: {error: "Not Found"}, status: :not_found
+  end
+
 end
--- spec/api/ping_spec.rb
+++ spec/api/ping_spec.rb
@@ -12,14 +12,33 @@ describe "Ping API" do
 
   it "returns the organization name" do
     create(:organization, name: "Test Organization", subdomain: "testorganization")
-
-    @request.host = "#{testorganization}.example.com"
+    host! "testorganization.example.com"
 
     get "/ping"
 
     expect(response).to be_success
     expect(json.ping).to eq "pong"
     expect(json.organization_name).to eq "Test Organization"
+  end
+
+  it "wont load any organization if the subdomain is on the list of non organization subdomains" do
+    host! "www.example.com"
+
+    get "/ping"
+
+    expect(response).to be_success
+    expect(json.ping).to eq "pong"
+    expect(json.organization_name).to be_nil
+  end
+
+  it "will return 404 if the organization does not exist" do
+    create(:organization, name: "Test Organization", subdomain: "testorganization")
+    host! "nonexistingorganization.example.com"
+
+    get "/ping"
+
+    expect(response).not_to be_success
+    expect(json.ping).to be_nil
   end
 
 end
