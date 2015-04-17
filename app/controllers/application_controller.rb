86b9b3f9df0770c59415f137e3ab0d9bfaf0d9c4
israel de la hoz <israeldelahoz@gmail.com>
2015-04-17 18:15:53 -0500

    refactor application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,28 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, :with => :not_found
   before_filter :fetch_organization
 
-  NON_ORGANIZATION_SUBDOMAINS = [:www]
-
   protected
 
   def fetch_organization
-    @organization = Organization.find_by! subdomain: request.subdomain if has_organization_subdomain?
+    @organization = Organization.find_by_subdomain!(request.subdomain) if has_organization_subdomain?
   end
 
   private
 
   def has_organization_subdomain?
-    request.subdomain.present? && !Settings.non_organization_subdomains.include?(request.subdomain)
+    has_subdomain? && !has_non_organization_subdomain?
   end
 
   def not_found
     render json: {error: "Not Found"}, status: :not_found
+  end
+
+  def has_subdomain?
+    request.subdomain.present?
+  end
+
+  def has_non_organization_subdomain?
+    Settings.non_organization_subdomains.include?(request.subdomain)
   end
 
 end
