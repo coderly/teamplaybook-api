93be81554734d428a2b3a45ea32042e0991f7df5
israel de la hoz <israeldelahoz@gmail.com>
2015-04-17 18:17:12 -0500

    add null validation at database level

--- db/migrate/20150416191351_create_organizations.rb
+++ db/migrate/20150416191351_create_organizations.rb
@@ -1,8 +1,8 @@
 class CreateOrganizations < ActiveRecord::Migration
   def change
     create_table :organizations do |t|
-      t.string :name
-      t.string :subdomain
+      t.string :name, null: false 
+      t.string :subdomain, null: false 
       t.timestamps null: false
     end
   end
