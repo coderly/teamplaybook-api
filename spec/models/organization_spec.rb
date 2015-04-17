aff82c929a77840940ad6075970811f98e41b9a3
israel de la hoz <israeldelahoz@gmail.com>
2015-04-17 18:18:38 -0500

    remove space on organization spec

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -4,7 +4,6 @@ describe Organization do
   describe "validations" do
     it "Should not allow to assign an empty subdomain" do
       organization = build(:organization, subdomain: "")
-
       expect(organization).not_to be_valid
     end
 
