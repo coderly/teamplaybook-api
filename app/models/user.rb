16e22e40a7ddc1e57354e01130db79278b4f3d23
israel de la hoz <israeldelahoz@gmail.com>
2015-04-21 18:22:31 -0500

    remove unused module

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :trackable, :validatable
 
   before_save :ensure_authentication_token
 
