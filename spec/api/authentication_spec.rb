a26b74554b4bd7e2cc58e96a825c63b6b55df43d
israel de la hoz <israeldelahoz@gmail.com>
2015-04-21 01:50:19 -0500

    add user registration

--- spec/api/authentication_spec.rb
+++ spec/api/authentication_spec.rb
@@ -27,23 +27,20 @@ describe "Authentication" do
     it "should create user" do
       post '/users', user: { email: 'test@test.com', password: 'password', password_confirmation: 'password' }
   
-      expect(json.user_email).to eq 'test@test.com'
-      expect(json.user_token).not_to be_nil
+      expect(json.data.authentication_token).not_to be_nil
+      expect(json.data.email).to eq 'test@test.com'
     end
 
     it "should authenticate with resulting token" do
       post '/users', user: { email: 'test@test.com', password: 'password', password_confirmation: 'password' }
-
-      token_authorize(json.user_token, json.user_email)
-      get '/me'
+      get '/me', {}, {"X-User-Email" => json.data.email, "X-User-Token" => json.data.authentication_token}
   
-      expect(json.user.registered).to be true
-      expect(json.user.email).to eq 'test@test.com'
+      expect(json.data.email).to eq 'test@test.com'
     end
 
     it "should return an errors in the correct format" do
        post '/users', user: { email: 'test@test.com', password: 'pas', password_confirmation: 'pas' }
-       expect(json.error).to eq "Email has already been taken and Password is too short (minimum is 8 characters)"
+       expect(json.error).to eq "Password is too short (minimum is 8 characters)"
     end
   end
 end
