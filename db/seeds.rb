d39b3ce9d2097eccbab225a2f62f3ccb734852e6
israel de la hoz <israeldelahoz@gmail.com>
2015-04-28 11:39:35 -0500

    refactor with scenario

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
+require 'team_playbook/scneario/create_or_update_plan'
 Settings.billing.plans.each do |plan_info|
-  plan = Plan.where(slug: plan_info.slug).first_or_initialize
-  plan.name = plan_info.name
-  plan.amount = plan_info.amount
-  plan.interval = plan_info.interval
-  plan.trial_period_days = plan_info.trial_period_days
-
-  plan.save!
+  TeamPlaybook::Scenario::CreateOrUpdatePlan.new.call(plan_info)
 end
---
+++ lib/team_playbook/scenario/create_or_update_plan.rb
@@ -0,0 +1,15 @@
+module TeamPlaybook
+  module Scenario
+    class CreateOrUpdatePlan
+      def call(plan_info)
+        plan = Plan.where(slug: plan_info.slug).first_or_initialize
+        plan.name = plan_info.name
+        plan.amount = plan_info.amount
+        plan.interval = plan_info.interval
+        plan.trial_period_days = plan_info.trial_period_days
+
+        plan.save!
+      end
+    end
+  end
+end
