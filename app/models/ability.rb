e631996e5ef1359a23fa4741d161e82b0ff95b0e
begedin <begedinnikola@gmail.com>
2015-05-07 14:34:22 +0200

    Ability model refactor

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,14 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user, team)
-    if team.owner ==  user
-      can :manage, TeamMembership, team: team
-    end
+    can :manage, TeamMembership, team: team if team.owner ==  user
     if team.members.include? user
       can :read, TeamMembership, team: team
-      if TeamMembership.find_by(team: team, user: user).has_any_role? :admin, :owner
-        can :update, TeamMembership, team: team
-      end
+      can :update, TeamMembership, team: team if TeamMembership.find_by(team: team, user: user).has_any_role? :admin, :owner
     end
   end
 end
