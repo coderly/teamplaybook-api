d2e5da90bb083abca2721a9231151a131f800ff0
begedin <begedinnikola@gmail.com>
2015-05-07 09:23:15 +0200

    More renaming

--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -2,7 +2,7 @@ class TeamMembership < ActiveRecord::Base
   belongs_to :team
   belongs_to :user
   validate :limit_role_to_invitee_for_unregistered_users,
-    :limit_role_to_owner_for_team_owners
+    :limit_role_to_owner_for_team_owner
 
   include RoleModel
   roles :invitee, :member, :admin, :owner
@@ -13,7 +13,7 @@ class TeamMembership < ActiveRecord::Base
     end
   end
 
-  def limit_role_to_owner_for_team_owners
+  def limit_role_to_owner_for_team_owner
     if team.present? and team.owner.present? and user == team.owner and !has_role? :owner
       errors.add(:role, "Cannot change role from owner.")
     end
