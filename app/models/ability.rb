95f2e358ceb43a9b3843c613908c3da7e0c757fa
begedin <begedinnikola@gmail.com>
2015-05-08 08:28:45 +0200

    Renamed `:manage` ability to `:create`
    This makes the ability system more constrained and hopefuly reduces the chances of us missing something

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user, team)
     if team.members.include? user
-      can :manage, TeamMembership, team: team if team.owner ==  user
+      can :create, TeamMembership, team: team if team.owner ==  user
       can :read, TeamMembership, team: team
       can :update, TeamMembership, team: team if TeamMembership.find_by(team: team, user: user).has_any_role? :admin, :owner
       can :destroy, TeamMembership, team: team if (team.owner === user)
