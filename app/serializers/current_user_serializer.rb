f75085a31e148018b744a9b8e94963b4da4d6df6
begedin <begedinnikola@gmail.com>
2015-05-11 07:53:45 +0200

    Fixes to current user serializer

--- app/serializers/current_user_serializer.rb
+++ app/serializers/current_user_serializer.rb
@@ -1,14 +1,13 @@
 class CurrentUserSerializer < ActiveModel::Serializer
   attributes :id, :email, :authentication_token, :role
 
-  belongs_to :current_team_membership, class_name: "TeamMembership"
-
-  def role
-    @options[:current_team_membership].roles.first.to_s unless @options[:current_team_membership].blank?
-  end
+  has_one :current_team_membership, serializer: TeamMembershipSerializer
 
   def current_team_membership
     @options[:current_team_membership] unless @options[:current_team_membership].blank?
   end
 
+  def role
+    current_team_membership.roles.first.to_s unless current_team_membership.blank?
+  end
 end
